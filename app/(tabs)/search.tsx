0f5a1749957bfff45e72dd758292e35b2146b9d9
Jarvis <home80jarvis@gmail.com>
2025-12-03 00:17:08 +0530

    Fix categories

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -52,17 +52,34 @@ const ProwlarrSearchScreen = () => {
             // Extract unique categories from all indexers
             const categoryMap = new Map<number, string>();
 
-            // Common category mappings
             const commonCategories: Record<number, string> = {
-                1000: 'Console',
-                2000: 'Movies',
-                3000: 'Audio',
-                4000: 'PC',
-                5000: 'TV',
-                6000: 'XXX',
-                7000: 'Books',
-                8000: 'Other',
+                // Movies
+                2000: "Movies",
+                2010: "Movies/Foreign",
+                2020: "Movies/Other",
+                2030: "Movies/SD",
+                2040: "Movies/HD",
+                2045: "Movies/UHD",
+                2050: "Movies/BluRay",
+                2060: "Movies/3D",
+                2070: "Movies/DVD",
+                2080: "Movies/WEB-DL",
+                2090: "Movies/x265",
+
+                // TV
+                5000: "TV",
+                5010: "TV/WEB-DL",
+                5020: "TV/Foreign",
+                5030: "TV/SD",
+                5040: "TV/HD",
+                5045: "TV/UHD",
+                5050: "TV/Other",
+                5060: "TV/Sport",
+                5070: "TV/Anime",
+                5080: "TV/Documentary",
+                5090: "TV/x265"
             };
+
 
             // First, add common categories
             Object.entries(commonCategories).forEach(([id, name]) => {
