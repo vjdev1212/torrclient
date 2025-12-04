b6f9930a0273cf07fc56360e832d4feb5c0ded6d
Jarvis <home80jarvis@gmail.com>
2025-12-04 23:17:32 +0530

    Fix library

--- app/(tabs)/library.tsx
+++ app/(tabs)/library.tsx
@@ -443,7 +443,11 @@ const styles = StyleSheet.create({
     flexGrow: 1,
   },
   emptyStateContainer: {
-    flex: 1,
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
     justifyContent: 'center',
     alignItems: 'center',
     paddingHorizontal: 40,
