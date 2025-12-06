1118a90ca5e824533c28fea98a41a11f3607be7b
Jarvis <home80jarvis@gmail.com>
2025-12-06 22:01:16 +0530

    Playback speed adjustment

--- components/coreplayer/index.tsx
+++ components/coreplayer/index.tsx
@@ -17,7 +17,7 @@ export const CONSTANTS = {
     CONTROLS_AUTO_HIDE_DELAY: 3000,
     CONTENT_FIT_LABEL_DELAY: 1000,
     SUBTITLE_UPDATE_INTERVAL: 50,
-    PLAYBACK_SPEEDS: [0.75, 0.8, 0.85, 0.9, 0.95, 1.0, 1.1, 1.15, 1.20, 1.25],
+    PLAYBACK_SPEEDS: [0.75, 0.8, 0.85, 0.9, 0.95, 1.0, 1.05, 1.1, 1.15, 1.20, 1.25],
     CONTENT_FIT_OPTIONS: ['contain', 'cover', 'fill'] as const
 };
 
